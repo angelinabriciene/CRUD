e9d5b3f1bf2d2bc5d68349becee8d661410274ea
angelinabriciene <aburbaite865@gmail.com>
2024-05-23 13:53:42 +0300

    "Studentų sąrašas" pagal CRUD + funkcijos

--- src/Main.java
+++ src/Main.java
@@ -24,40 +24,16 @@ public class Main {
             sc.nextLine();
             switch (name) {
                 case 1:
-                    System.out.println("Įveskite vardą");
-                    String input = sc.nextLine();
-                    if (studentList.contains(input)) {
-                        System.out.println("Toks vardas jau yra");
-                    } else studentList.add(input);
+                    newStudent(studentList, sc);
                     break;
                 case 2:
-                    System.out.println("Studentai:");
-                    for (int i = 0; i < studentList.size(); i++) {
-                        System.out.println(studentList.get(i));
-                    }
+                    existingStudents(studentList);
                     break;
                 case 3:
-                    System.out.println("Įveskite vardą, kurį norite redaguoti");
-                    String oldName = sc.nextLine();
-                    if (studentList.contains(oldName)) {
-                        System.out.println("Įveskite naują vardą");
-                        String newName = sc.nextLine();
-                        int index = studentList.indexOf(oldName);
-                        studentList.set(index, newName);
-                        System.out.println("Vardas pakeistas iš " + oldName + " į " + newName);
-                    } else {
-                        System.out.println("Tokio vardo nėra");
-                    }
+                    editStudent(studentList);
                     break;
                 case 4:
-                    System.out.println("Įveskite vardą, kurį norite ištrinti");
-                    String wrongName = sc.nextLine();
-                    if (studentList.contains(wrongName)) {
-                        studentList.remove(wrongName);
-                        System.out.println("Vardas ištrintas");
-                    } else {
-                        System.out.println("Tokio vardo nėra");
-                    }
+                    removeStudent(studentList);
                     break;
                 case 5:
                     System.exit(1);
@@ -68,6 +44,48 @@ public class Main {
 
 //        PABAIGA
 
+    }
+
+    public static void existingStudents(ArrayList<String> studentList) {
+        System.out.println("Studentai:");
+        for (int i = 0; i < studentList.size(); i++) {
+            System.out.println(studentList.get(i));
+        }
+    }
+
+    public static void newStudent(ArrayList<String> studentList, Scanner sc) {
+        System.out.println("Įveskite vardą");
+        String input = sc.nextLine();
+        if (studentList.contains(input)) {
+            System.out.println("Toks vardas jau yra");
+        } else studentList.add(input);
+    }
+
+    public static void editStudent(ArrayList<String> studentList) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Įveskite vardą, kurį norite redaguoti");
+        String oldName = sc.nextLine();
+        if (studentList.contains(oldName)) {
+            System.out.println("Įveskite naują vardą");
+            String newName = sc.nextLine();
+            int index = studentList.indexOf(oldName);
+            studentList.set(index, newName);
+            System.out.println("Vardas pakeistas iš " + oldName + " į " + newName);
+        } else {
+            System.out.println("Tokio vardo nėra");
+        }
+    }
+
+    public static void removeStudent(ArrayList<String> studentList) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Įveskite vardą, kurį norite ištrinti");
+        String wrongName = sc.nextLine();
+        if (studentList.contains(wrongName)) {
+            studentList.remove(wrongName);
+            System.out.println("Vardas ištrintas");
+        } else {
+            System.out.println("Tokio vardo nėra");
+        }
     }
 
 
